fba5c574ded0440bfc8bb1dc01bdc9faadbc911c
Justin <legoguy191@hotmail.com>
2020-03-09 15:19:44 -0500

    Added files excluding password.php

---
+++ about/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Blog</title>
+            <link rel="stylesheet" type="text/css" href="/responsive-design.css">
+    </head>
+    <body>
+        <div id="wrapper">
+      
+            
+            <?php include $_SERVER['DOCUMENT_ROOT']."/templates/header.php"?>
+            <div id="main-body">
+               <p> Yo I'm Justin. </p>
+            </div>
+        
+            <?php include $_SERVER['DOCUMENT_ROOT']."/templates/footer.php"?>
+        </div>
+    </body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Welcome Home</title>
+            <link rel="stylesheet" type="text/css" href="responsive-design.css">
+            
+            <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png">
+            <link rel="icon" type="image/favicons/png" sizes="96x96" href="images/favicon-96x96.png">
+            <link rel="icon" type="image/favicons/png" sizes="16x16" href="images/favicon-16x16.png">
+    </head>
+    <body>
+        <div id="wrapper">  
+           
+            <header class="header-main">
+
+                <?php include $_SERVER['DOCUMENT_ROOT']."/templates/header.php"?>
+
+            </header>
+
+        
+            
+            <div class='prev-bar'>
+            
+                    <a href="http://home.com" class="icon" style="background-image: url('images/youtube.png');">youtube</a>
+                    <a href="http://home.com" class="icon" style="background-image: url('images/instagram.png');">instagram</a>
+                    <a href="http://home.com" class="icon" style="background-image: url('images/twitter.png');">twitter</a>
+            </div>
+
+            <div class='intro-section'>
+                <div class="one-column-box">
+                    <h2 class="text-centered">Justin Petkus</h2>
+                        <p> Welcome everyone, and thanks for visiting my site! I'ts still pretty rough around the edges but this is set to be my
+                            little home on the large map of the web. You may be wondering a bit on what this site is about and who I am. In short
+                            I am a college student and a modern day Renaissance man. For more info you can always check the 'blog' and 'about me'
+                            pages. Thanks again for coming, and enjoy your stay! 
+                        </p>
+                </div>
+            </div>
+        
+            <?php include $_SERVER['DOCUMENT_ROOT']."/templates/footer.php"?>
+        </div>  
+        
+    </body>
+ 
+
+
+  
